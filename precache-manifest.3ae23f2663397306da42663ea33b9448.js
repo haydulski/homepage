53849da2ec0bca21644ba6afbe0e8221b7a19f7a
Damian <dhfxblog@gmail.com>
2020-06-24 12:19:35 +0200

    Updates

---
+++ precache-manifest.3ae23f2663397306da42663ea33b9448.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd1cca362f1106a06bf7c7ce7a9f5966",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ce57a453c90289cd820",
+    "url": "/static/css/main.1dc783d3.chunk.css"
+  },
+  {
+    "revision": "8b4b52c6af4e076cedcf",
+    "url": "/static/js/2.7338f74a.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.7338f74a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ce57a453c90289cd820",
+    "url": "/static/js/main.cad18d2e.chunk.js"
+  },
+  {
+    "revision": "bdd01ac27bde8efb762e",
+    "url": "/static/js/runtime-main.88310318.js"
+  },
+  {
+    "revision": "e2957fa797da65cc06f79bfe8b216ff3",
+    "url": "/static/media/damian1.e2957fa7.png"
+  },
+  {
+    "revision": "c9c59f0345a958fbb19e019500732f4c",
+    "url": "/static/media/gory4linia.c9c59f03.png"
+  },
+  {
+    "revision": "a8779a6c2f4af056e215e2a8385629ef",
+    "url": "/static/media/hobby1.a8779a6c.jpg"
+  },
+  {
+    "revision": "c02ef9942556fe46c6392e34071a9ae8",
+    "url": "/static/media/hobby2.c02ef994.jpg"
+  },
+  {
+    "revision": "6a7c34e1eecb42936a25519ef55db1fe",
+    "url": "/static/media/hobby3.6a7c34e1.jpg"
+  },
+  {
+    "revision": "4ab1fa6cc7dac9ce18fcfd23f3cdb5c6",
+    "url": "/static/media/humantest.4ab1fa6c.jpg"
+  },
+  {
+    "revision": "301440d1d0c6558d36ac453712c321af",
+    "url": "/static/media/las1linia.301440d1.png"
+  },
+  {
+    "revision": "0c99e922bcccff553a7a67c9a8d16dcb",
+    "url": "/static/media/las2linia.0c99e922.png"
+  },
+  {
+    "revision": "3c9e1fb9f763f9ff3a5209866335ef0d",
+    "url": "/static/media/niebo3linia.3c9e1fb9.png"
+  },
+  {
+    "revision": "a26755590560f4700e2062ba965bcc01",
+    "url": "/static/media/niebo5linia.a2675559.png"
+  },
+  {
+    "revision": "e40add3cb8ceab1d9e324f4c81cbf731",
+    "url": "/static/media/programy.e40add3c.png"
+  },
+  {
+    "revision": "0ed6361e6adfaac516ce5b5d5963523f",
+    "url": "/static/media/rwdlogo.0ed6361e.png"
+  },
+  {
+    "revision": "d5023c54a2f491b256a3d92f5cba25c1",
+    "url": "/static/media/seologo.d5023c54.png"
+  },
+  {
+    "revision": "97482000c2ad87c61f87a7270388183a",
+    "url": "/static/media/strona-github.97482000.jpg"
+  },
+  {
+    "revision": "8c5a9f11da3461e414f5fcf4374e959f",
+    "url": "/static/media/strona-kopernik.8c5a9f11.jpg"
+  },
+  {
+    "revision": "3158aab1a36df7746d72bafa2b92f775",
+    "url": "/static/media/strona-premia.3158aab1.png"
+  }
+]);
